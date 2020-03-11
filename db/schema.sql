41112196b9d4a132fe0c6d526adf4247fab3ff0b
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-03-11 14:55:56 -0500

    my changes

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+--Drops the task_saver_db if it already exists --
+DROP DATABASE IF EXISTS burger_db
+
+--Create Burger Table--
+Create TABLE `burgers_db`.
+   
+
+   CREATE TABLE burgers (
+  id INT NOT NULL AUTO INCREMENT,
+  burger_name:  string.
+ 
+  name VARCHAR(30) NOT NULL,
+  PRIMART KEY (id),
+);
