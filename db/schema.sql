080a3be7040717b95a4110d94d91d6420378d857
Vandana Sivadasan <sivadasanvandana62@gmail.com>
2020-04-02 09:56:27 -0500

    Adding contents in sql

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+###Schema
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers_db
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar() NOT NULL,
+    devoured varchar () boolean
+    PRIMARY KEY (id)
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+----Insert set of burgers into the table
+INSERT INTO burgers (name)
+VALUES ("Cheese Burger");
+
+INSERT INTO burgers (name)
+VALUES ("Chicken Burger");
+
+INSERT INTO burgers (name)
+VALUES ("Hamburger");
+
+INSERT INTO burgers (name)
+VALUES ("Black Bean Burger");
